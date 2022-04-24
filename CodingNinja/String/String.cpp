e5737affcbb0c899d5f1154ebb5b97bf0c85c0ce
Veer Chauhan <r.ranjanchn@gmail.com>
2022-04-25 01:04:56 +0530

    Check number is palindrom or not

--- CodingNinja/String/String.cpp
+++ CodingNinja/String/String.cpp
@@ -28,16 +28,59 @@ int count_char(char *arr, int size) {
 // Check a string is palindrom or not
 
 
-bool check_palindrom(char *input) {
+bool check_string_palindrom(char *input) {
     
     bool isPalindrom;
+    
+    
+    // abcdbca -> false
+    // abcdcba  -> True
+    // a ->  true
+    // aba -> true
+    // ab -> false
     
     
     return isPalindrom;
 }
 
+
+// Check a Number is palindrom or not
+
+
+bool check_number_palindrom(int input) {
+    
+    bool isPalindrom = false;
+    
+    
+    // 11111111 -> true
+    // 121  -> True
+    // 1 ->  true
+    // 1234213 -> false
+    // 2133321 -> false
+    
+    
+    int verifier  = input;
+    
+    int data = 0;
+    while (input > 0) {
+        int num = input % 10;
+        input = input / 10;
+        data = data * 10 + num;
+    }
+    if (verifier == data) {
+        isPalindrom = true;
+    }
+    return isPalindrom;
+}
+
+
+
 int main() {
     char name [100];
+    int number;
+    cout << "Enter number to check it is palindrom or not : ";
+    cin >> number;
+    cout << check_number_palindrom(number) <<" number is " << check_number_palindrom(number)<< " palindrom " << endl;
     cout << "Enter your name : ";
     cin >> name ;
     cout << "Address of Name " << &name << endl;
