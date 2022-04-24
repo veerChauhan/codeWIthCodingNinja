50f43f5e4a25f01230fa64f3466609e08323d53f
Veer Chauhan <r.ranjanchn@gmail.com>
2022-04-25 01:50:57 +0530

    Explaination added

--- CodingNinja/String/check_string_palindrom.cpp
+++ CodingNinja/String/check_string_palindrom.cpp
@@ -10,35 +10,17 @@
 #include "Count_charecter_in_String.hpp"
 using namespace std;
 
-bool check_string_palindrom_Approach(char *input) {
-    
-    bool isPalindrom = false;
-    int size = count_char(input);
-    // naive approach
-    
-    
-    isPalindrom = true;
-        
-        
-        
-    // abcdbca -> falsee
-    // abcdcba  -> True
-    // a ->  true
-    // aba -> true
-    // ab -> false
-    
-    
-    return isPalindrom;
-}
-
 
 bool check_string_palindrom(char *input) {
     
+    // Take a variable to return the result
     bool isPalindrom = false;
-    int size = count_char(input);
-    // naive approach
     
-    // check from start and end with size n/2
+    //get the size of string first by counting number of charecter untill null charecter found
+    // for(int i = 0; input[i] == '\0' ; i++)
+    int size = count_char(input);
+    // check from start and end with size n/2 so that we can move  half form left and half from right
+    // odd charecter count and single charecter case are handled from here int j = size - i - 1
     for (int i = 0; i < size / 2; i++) {
         int j = size - i - 1 ;
         cout << input[i] << " and " << input[j] << endl;
